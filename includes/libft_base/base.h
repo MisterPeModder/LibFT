c067171a1398b78f4262f2a62cbd8d58c95f3ae9
Yanis GUAYE <yguaye@student.42.fr>
2017-12-22 11:58:48 +0100

    Obey the norm!

--- includes/libft_base/base.h
+++ includes/libft_base/base.h
@@ -6,21 +6,17 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/10 12:32:10 by yguaye            #+#    #+#             */
-/*   Updated: 2017/12/21 14:46:42 by yguaye           ###   ########.fr       */
+/*   Updated: 2017/12/22 11:57:47 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_H
 # define BASE_H
 
-/**Deprecated: please use the other include files!**/
-
 # include "character.h"
 # include "io.h"
 # include "list.h"
 # include "memory.h"
 # include "stringft.h"
-
-/**Deprecated: please use the other include files!**/
 
 #endif
