c3ad03377eac5ee7c17a3e8c7c5d04871dbe3152
Yanis GUAYE <yguaye@student.42.fr>
2018-01-05 09:02:45 +0100

    Fix memory leak when deleting binary tree

--- srcs/base/btree/ft_btreedel.c
+++ srcs/base/btree/ft_btreedel.c
@@ -6,7 +6,7 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/04 10:09:24 by yguaye            #+#    #+#             */
-/*   Updated: 2018/01/05 08:52:27 by yguaye           ###   ########.fr       */
+/*   Updated: 2018/01/05 09:01:26 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void				ft_btreedel(t_btree **tree, void (*del)(void *, size_t))
 {
 	if ((*tree)->left && !IS_LEAF((*tree)->left))
 		ft_btreedel(&(*tree)->left, del);
-	ft_btreedelone(tree, del);
 	if (*tree && (*tree)->right && !IS_LEAF((*tree)->right))
 		ft_btreedel(&(*tree)->right, del);
+	ft_btreedelone(tree, del);
 }
 
 void				ft_btreedelone(t_btree **node, void (*del)(void *, size_t))
