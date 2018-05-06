b7bb4a6bfbdd29a058d3455bf48da8912fddba96
Yanis Guaye <yguaye@student.42.fr>
2018-05-06 18:14:22 +0200

    Remove sneaky printf

--- srcs/base/list/ft_lstextract.c
+++ srcs/base/list/ft_lstextract.c
@@ -6,12 +6,11 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/17 13:45:51 by yguaye            #+#    #+#             */
-/*   Updated: 2017/12/17 14:40:22 by yguaye           ###   ########.fr       */
+/*   Updated: 2018/05/06 18:14:01 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_base/base.h"
-#include <stdio.h>
 
 t_list				*ft_lstextract(t_list **lst, void *target,
 		int (*cmp)(void *, void *))
@@ -25,7 +24,6 @@ t_list				*ft_lstextract(t_list **lst, void *target,
 	{
 		if (!(*cmp)(target, tmp->content))
 		{
-			printf("rem!");
 			ft_lstrem(lst, tmp);
 			tmp->next = NULL;
 			return (tmp);
